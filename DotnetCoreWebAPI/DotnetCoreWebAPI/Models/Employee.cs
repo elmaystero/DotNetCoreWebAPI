a609aa4eecd61e243735b43dea5b072a6bfd3273
Mohammed.Osman <Mohammed.Osman@EZZAT-20070>
2021-06-25 21:20:16 +0400

    update the api

---
+++ DotnetCoreWebAPI/DotnetCoreWebAPI/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotnetCoreWebAPI.Models
+{
+    public partial class Employee
+    {
+        public Employee()
+        {
+        }
+
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
